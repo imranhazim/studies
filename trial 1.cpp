56a24fad2076bf843438689f4a293406702fe1a9
imranhazim <i.hazimsalim@live.iium.edu.my>
2021-10-12 20:04:11 +0800

    Create trial 1.cpp

---
+++ trial 1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	return 0;
+}
+
+
+int main (){
+	const int b =3;
+	int a;
+	float  t= 4.34;
+	double y= 4.34;
+	char i= 'a';
+//	string name = "Arif";
+//	bool statement = true;
+//	bool lies = false;
+//	bool
+//	cout << "Insert a number \n";
+//	cin >> a;
+//	cout <<"number inserted: " << a;
+//    cout << "float :" << t;
+//    cout << "\ndouble :" << y;
+//    cout << "Char :" << i <<endl;
+//    cout << "String :" << name;
+//    cout << "statement :" << statement ;
+//    cout << "\nlies :" << lies ;
+    cout << b << ;
+
+
+    
+	return 0;
+}
